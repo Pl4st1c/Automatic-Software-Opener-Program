86d1f88b38e657fed8a22dd6dcd5bcb89f00d4c1
Riccardo <99977798+Pl4st1c@users.noreply.github.com>
2023-03-16 12:13:49 +0100

    Add files via upload

---
+++ TFFOC_Multiplayer_key_runner/TFFOC_Multiplayer_runner/src/PSO.java
@@ -0,0 +1,20 @@
+import java.io.*;
+public class PSO {
+public static void main(String args[]) {
+	
+	Runtime.getRuntime();
+	Runtime runtime = Runtime.getRuntime();
+	try {
+		runtime.exec("notepad");//TFFOC OPENER 
+		Process p = Runtime.getRuntime().exec("C:\\Program Files\\Google\\Chrome\\Application\\chrome");//KEYGEN_OPENER
+		//for pirated copies change the path for Process p
+		System.out.println("The softwares are running correctly!");
+	}catch(IOException ioe) {
+		System.out.println("Exception: "+ioe);
+	}
+	}
+	
+	
+	
+}
+
